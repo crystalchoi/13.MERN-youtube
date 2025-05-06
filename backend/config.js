d2fa749c3b0c95f2e57a65fe704a8262fcf7295f
crystalchoi <sujeungchoi@gmail.com>
2025-05-06 16:38:31 +0900

    first commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+export const PORT = 8000
+export const MongoDB_URL = "mongodb+srv://admin:admin@cluster0.z0fkbmx.mongodb.net/Book?retryWrites=true&w=majority&appName=Cluster0"
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {MongoDB_URL, PORT} from './config.js'
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	// console.log(req.query);
+	res.status(234).send("MERN App Start");
+})
+
+mongoose.connect(MongoDB_URL)
+	.then(() => {
+	console.log("MongoDB Connected");
+	app.listen(PORT, () => {
+		console.log(`Backend start at Port ${PORT}`)
+	})
+}).catch((err) => {
+	console.log(err);
+})
